a2182dd091b6170f4f3d951d27066663ff8ef269
Javier Zarate <zarate.javier.jz@gmail.com>
2020-04-16 18:34:46 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+/*////////////////////////////////These are the variables////////////////////////////////////////// */
+window.addEventListener("load", listen);
+
+var images = new Array(
+  'images/IMG_0725.JPG',
+  'images/IMG_0726.JPG',
+  'images/IMG_0727.JPG',
+  'images/IMG_0728.JPG',
+);
+var timer
+var index = 0
+
+/* ////////////////////////////////////////Event listeners////////////////////////////////////// */
+function listen(){
+  document.getElementById('next').addEventListener("click", changePhoto);
+  document.getElementById('prev').addEventListener("click", previous);
+}
+
+/* ///////////////////////////////Functions for event listeners///////////////////////////////// */
+
+function changePhoto() {
+  index++;
+  if (index >= images.length) {
+    index = 0;
+  }
+  // document.getElementById("photo").innerHTML.src = images[index]
+  document.getElementById("photo").src = images[index];
+
+}
+
+function previous() {
+  index--;
+  if (index < 0) {
+    index = images.length - 1;
+  }
+
+  // document.getElementById("photo").innerHTML.src = images[index]
+  document.getElementById("photo").src = images[index];
+
+
+}
+
+
